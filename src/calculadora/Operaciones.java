78d8950cbae0a519a90e41611a74fc8a1eadf5ea
APRENDIZ <domain1@domain1.com>
2017-04-28 09:57:37 -0500

    Version 0.2

--- src/calculadora/Operaciones.java
+++ src/calculadora/Operaciones.java
@@ -5,10 +5,45 @@
  */
 package calculadora;
 
+import java.util.Scanner;
+
 /**
  *
  * @author APRENDIZ
  */
 public class Operaciones {
+
+    public int numero1;
+    public int numero2;
+    public int cantidad;
+    public int resultado;
     
+    public void Sumar(){
+    
+    Scanner reader = new Scanner(System.in);
+    
+    System.out.println("Eliga La Cantidad De Sumas que desea Realizar: ");
+    cantidad = reader.nextInt();
+    
+    
+        if (cantidad<=10) {
+            for (int i = 0; i < cantidad; i++) {
+    System.out.println("Digite El Primer Número: ");
+    numero1 = reader.nextInt();
+    System.out.println("Digite El segundo Número: ");
+    numero2 = reader.nextInt();
+                resultado = numero1 + numero2;
+                System.out.println("Numero de Suma: "+i);
+                System.out.println("El Resultado Es: "+resultado);
+            }
+            
+        }else {
+        
+        System.out.println("Cantidad De Números Invalida....");
+        
+        }
+    
+    
+    }
+
 }
