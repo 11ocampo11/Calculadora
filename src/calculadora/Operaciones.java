124321b920a285442c76be3ecc998d305513012d
mospinar <mauricio.77950@gmail.com>
2017-04-28 11:24:38 -0500

    version 0.2

--- src/calculadora/Operaciones.java
+++ src/calculadora/Operaciones.java
@@ -5,16 +5,35 @@
  */
 package calculadora;
 
+import java.util.Scanner;
+
 /**
  *
  * @author APRENDIZ
  */
 public class Operaciones {
+    Scanner sc = new Scanner(System.in);
     public int numero1;
     public int numero2;
     public int cantidad;
     public int resultado;
     
-    
+    public void multiplicar(){
+        System.out.println("Seleccione la cantidad de operaciones");
+        cantidad = sc.nextInt();
+        if (cantidad > 5){
+            System.out.println("Maximo 5 operaciones");
+        } else {
+            for (int i = 0; i < cantidad; i++) {
+                System.out.println("Operacion numero " + i);
+                System.out.print("numero 1: ");
+                numero1 = sc.nextInt();
+                System.out.print("numero 2: ");
+                numero2 = sc.nextInt();
+                resultado = numero1 * numero2;
+                System.out.println("El resultado de la operacion "+ i + " es: " + resultado);
+            }
+        }
+    }
    
 }
